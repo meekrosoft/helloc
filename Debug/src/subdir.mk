5712515a3c7526f11e634d3fa222ee2c868df043
Alison Chaiken <alison@she-devel.com>
2020-03-11 16:45:15 -0700

    Add pthread to explicit dependencies of helloc
    On Linux systems with glibc, the linker fails to find libpthread
    without an explicit mention in the makefile.

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -12,12 +12,14 @@ OBJS += \
 CPP_DEPS += \
 ./src/helloc_testsuite.d 
 
+GTEST_DIR = $(HOME)/gitsrc/googletest/googletest
+CFLAGS = -isystem $(GTEST_DIR)/include
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ $(CFLAGS) -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
